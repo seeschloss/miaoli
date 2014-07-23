775bd23f0475b12747f18f1540668d70903dd36e
SeeSchloss <seeschloss@seos.fr>
2014-07-23 10:46:51 +0200

    Missing semi-colons

--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,7 @@
 exports.home = function(req, res){
-  res.render('home', { title: 'Home' })
+  res.render('home', { title: 'Home' });
 };
 
 exports.tribune = function(req, res){
-  res.render('tribune', { title: req.tribune.title, tribune: req.tribune, user: req.user })
+  res.render('tribune', { title: req.tribune.title, tribune: req.tribune, user: req.user });
 };
