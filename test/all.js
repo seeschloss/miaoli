2dca06edac5e7ad80c408d000a1f8ed2f42a9e00
SeeSchloss <see@seos.fr>
2021-12-02 11:49:53 +0000

    Rewrite from scratch

--- test/all.js
+++
@@ -1,6 +0,0 @@
-exports["test database"] = require('./db.js');
-exports["test tribune"] = require('./tribune.js');
-exports["test user"] = require('./user.js');
-
-if (module == require.main) require('test').run(exports);
-
